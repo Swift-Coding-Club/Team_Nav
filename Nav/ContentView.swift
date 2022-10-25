d8ccbf3710110c43cdd2f1288969deec4212ce3e
taek0622 <min4644@gmail.com>
2022-10-26 00:45:54 +0900

    Feat:sparkles:: #1 Add Project

---
+++ Nav/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Nav
+//
+//  Created by 김민택 on 2022/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
