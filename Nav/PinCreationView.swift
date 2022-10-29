bb44bf5cc0115516da7fa695289137a159e8ffd4
taek0622 <min4644@gmail.com>
2022-10-29 15:50:38 +0900

    Chore:wrench:: #5 Add PinCreationView File
    - PinCreationView 파일 생성

---
+++ Nav/PinCreationView.swift
@@ -0,0 +1,21 @@
+//
+//  PinCreationView.swift
+//  Nav
+//
+//  Created by 김민택 on 2022/10/29.
+//
+
+import SwiftUI
+
+struct PinCreationView: View {
+    var body: some View {
+        VStack {
+        }
+    }
+}
+
+struct PinCreationView_Previews: PreviewProvider {
+    static var previews: some View {
+        PinCreationView()
+    }
+}
