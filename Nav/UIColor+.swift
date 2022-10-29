d8d093d6282672df1a91c6c0e434647115682e6f
taek0622 <min4644@gmail.com>
2022-10-29 16:14:00 +0900

    Feat:sparkles:: #6 Add UIColor Extension
    - Asset에 추가한 컬러를 UIColor extension에 추가

--- Nav/UIColor+.swift
+++ Nav/UIColor+.swift
@@ -8,4 +8,10 @@
 import SwiftUI
 
 extension UIColor {
+    class var primary: UIColor? { return UIColor(named: "PrimaryRed") }
+    class var secondary: UIColor? { return UIColor(named: "SecondaryOrange") }
+    class var redBean: UIColor? { return UIColor(named: "RedBean") }
+    class var navBlack: UIColor? { return UIColor(named: "NavBlack") }
+    class var navGray: UIColor? { return UIColor(named: "NavGray") }
+    class var navWhite: UIColor? { return UIColor(named: "NavWhite") }
 }
