d4159fa9549cfc486bb18fcb121c60f01ca8757b
taek0622 <min4644@gmail.com>
2022-10-30 20:33:14 +0900

    Feat:sparkles:: #6 Modify Color Extension

---
+++ Nav/Color+.swift
@@ -0,0 +1,17 @@
+//
+//  Color+.swift
+//  Nav
+//
+//  Created by 김민택 on 2022/10/29.
+//
+
+import SwiftUI
+
+extension Color {
+    static let primary = Color("PrimaryRed")
+    static let secondary = Color("SecondaryOrange")
+    static let redBean = Color("RedBean")
+    static let navBlack = Color("NavBlack")
+    static let navGray = Color("NavGray")
+    static let navWhite = Color("NavWhite")
+}
