9fe6190b48a9600b0fc344158e67257298e02b9e
taek0622 <min4644@gmail.com>
2022-10-30 22:53:45 +0900

    Chore:wrench:: #5 Modify Code Structure
    - VStack의 배경색을 ZStack으로 구조 변경
    - 디자인 시스템에 맞게 색상 변경

--- Nav/PinCreationView.swift
+++ Nav/PinCreationView.swift
@@ -14,130 +14,138 @@ struct PinCreationView: View {
     @State private var locationDescription: String = ""
     
     var body: some View {
-        VStack {
-            HStack {
-                Button(action: {}) {
-                    Image(systemName: "xmark")
-                        .resizable()
-                        .frame(width: 15, height: 15)
-                        .foregroundColor(.red)
-                        .frame(width: 40, height: 40)
-                        .background(
-                            Circle().fill(.white)
-                        )
-                }
-                
-                Spacer()
-                
-                Text("핀 추가")
-                    .foregroundColor(.white)
-                    .font(.system(size: 18, weight: .semibold))
-                
-                Spacer()
-                
-                Spacer()
-                    .frame(width: 40)
-            }
-            .padding([.leading, .trailing], 16)
-            
-            VStack(alignment: .leading) {
+        ZStack {
+            Color.primary
+                .ignoresSafeArea()
+            VStack {
                 HStack {
-                    Text("테마")
-                        .font(.system(size: 16, weight: .semibold))
+                    Button(action: {}) {
+                        Image(systemName: "xmark")
+                            .resizable()
+                            .frame(width: 15, height: 15)
+                            .foregroundColor(.primary)
+                            .padding((40 - 15) / 2)
+                            .background(
+                                Circle()
+                                    .fill(Color.navWhite)
+                            )
+                    }
                     
                     Spacer()
                     
-                    Button(action: {}) {
-                        Text(locationCategory)
-                            .padding(8)
-                            .foregroundColor(.black)
-                            .background(RoundedRectangle(cornerRadius: 16).fill(.gray))
-                    }
-                }
-                
-                HStack {
-                    Text("이름")
-                        .font(.system(size: 16, weight: .semibold))
+                    Text("핀 추가")
+                        .headline()
+                        .foregroundColor(.white)
                     
-                    VStack {
-                        TextField("추가할 장소의 이름을 입력해주세요.", text: $locationName)
-                        
-                        Divider()
-                    }
-                    .padding(.leading, 16)
-                }
-                
-                HStack {
-                    Text("주소")
-                        .font(.system(size: 16, weight: .semibold))
+                    Spacer()
                     
-                    VStack {
-                        TextField("클릭해서 주소를 변경해주세요.", text: $locationAddress)
-                        
-                        Divider()
-                    }
-                    .padding(.leading, 16)
+                    Spacer()
+                        .frame(width: 40)
                 }
+                .padding([.leading, .trailing], 16)
                 
-                Group {
-                    Text("사진")
-                        .font(.system(size: 16, weight: .semibold))
-                    
+                VStack(alignment: .leading) {
                     HStack {
+                        Text("테마")
+                            .subhead3()
+                            .foregroundColor(.navBlack)
+                        
+                        Spacer()
+                        
                         Button(action: {}) {
-                            Image(systemName: "plus.circle.fill")
-                                .resizable()
-                                .frame(width: 30, height: 30)
-                                .foregroundColor(.red)
-                                .frame(width: 110, height: 110)
-                                .background(
-                                    RoundedRectangle(cornerRadius: 16)
-                                        .fill(.gray)
-                                )
+                            Text(locationCategory)
+                                .body2()
+                                .foregroundColor(.navBlack)
+                                .padding(8)
+                                .background(RoundedRectangle(cornerRadius: 16).fill(Color.navGray))
                         }
                     }
-                }
-                
-                Group {
-                    Text("설명")
-                        .font(.system(size: 16, weight: .semibold))
                     
-                    TextEditor(text: $locationDescription)
-                        .cornerRadius(16)
-                }
-                
-                Group {
-                    Text("평점")
-                        .font(.system(size: 16, weight: .semibold))
-                        .frame(height: 30)
+                    HStack {
+                        Text("이름")
+                            .subhead3()
+                            .foregroundColor(.navBlack)
+                        
+                        VStack {
+                            TextField("추가할 장소의 이름을 입력해주세요.", text: $locationName)
+                            
+                            Divider()
+                        }
+                        .padding(.leading, 16)
+                    }
                     
-                    Text("aa")
+                    HStack {
+                        Text("주소")
+                            .subhead3()
+                            .foregroundColor(.navBlack)
+                        
+                        VStack {
+                            TextField("클릭해서 주소를 변경해주세요.", text: $locationAddress)
+                            
+                            Divider()
+                        }
+                        .padding(.leading, 16)
+                    }
                     
-                    // TODO: - 하단의 Spacer를 별점으로 변경
+                    Group {
+                        Text("사진")
+                            .subhead3()
+                            .foregroundColor(.navBlack)
+                        
+                        HStack {
+                            Button(action: {}) {
+                                Image(systemName: "plus.circle.fill")
+                                    .resizable()
+                                    .frame(width: 30, height: 30)
+                                    .foregroundColor(.primary)
+                                    .padding(40)
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 16)
+                                            .fill(Color.navGray)
+                                    )
+                            }
+                        }
+                    }
                     
-                    Spacer()
-                        .frame(width: 60, height: 60)
+                    Group {
+                        Text("설명")
+                            .subhead3()
+                            .foregroundColor(.navBlack)
+                        
+                        TextEditor(text: $locationDescription)
+                            .cornerRadius(16)
+                    }
+                    
+                    Group {
+                        Text("평점")
+                            .subhead3()
+                            .foregroundColor(.navBlack)
+                        
+                        // TODO: - 하단의 Spacer를 별점으로 변경
+                        
+                        Spacer()
+                            .frame(width: 60, height: 60)
+                    }
+                    
+                    Button(action: {}) {
+                        Text("확인")
+                            .subhead3()
+                            .foregroundColor(.navWhite)
+                            .frame(maxWidth: .infinity, maxHeight: 50)
+                            .background(
+                                RoundedRectangle(cornerRadius: 16)
+                                    .fill(Color.primary)
+                            )
+                    }
+                    .padding(.bottom, 16)
                 }
-                
-                Button(action: {}) {
-                    Text("확인")
-                        .font(.system(size: 18, weight: .semibold))
-                        .foregroundColor(.white)
-                        .frame(width: UIScreen.main.bounds.width - 32, height: 50)
-                        .background(RoundedRectangle(cornerRadius: 16).fill(.red))
-                }
-                
-                Spacer()
+                .padding(16)
+                .frame(maxWidth: .infinity, maxHeight: UIScreen.main.bounds.height * 0.9)
+                .background(Color.secondary)
+                .cornerRadius(16)
+                .ignoresSafeArea()
             }
-            .padding(16)
-            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.88)
-            .background(
-                RoundedRectangle(cornerRadius: 16)
-                    .fill(.orange)
-            )
         }
-        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height, alignment: .bottom)
-        .background(.red)
     }
 }
 
