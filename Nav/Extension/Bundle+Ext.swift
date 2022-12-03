51eeb22c46adf63345fc7d3e1a5b87fa838954f9
tawans <tawans@naver.com>
2022-12-03 15:54:44 +0900

    Asher: base setting commit

---
+++ Nav/Extension/Bundle+Ext.swift
@@ -0,0 +1,27 @@
+//
+//  Bundle+Ext.swift
+//  Nav
+//
+//  Created by 박재경 on 2022/11/03.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode<T: Decodable>(file: String) -> T {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Could not find \(file) in project")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Could not find \(file) in project")
+        }
+        
+        let decoder = JSONDecoder()
+        guard let loadData = try? decoder.decode(T.self, from:data) else {
+            fatalError("Could not find \(file) in project")
+        }
+        
+        return loadData
+    }
+}
