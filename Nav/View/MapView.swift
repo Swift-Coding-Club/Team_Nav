57f4b3b0187a8e69d33ff3eac735aa1b7745cc82
taek0622 <min4644@gmail.com>
2022-12-14 15:52:16 +0900

    Feat:sparkles:: #18 Add Login alert func
    - 로그인 / 비로그인 상태에 따라 뷰를 이동하거나, 경고창을 띄울 수 있도록 버튼을 바꾸는 기능 추가

--- Nav/View/MapView.swift
+++ Nav/View/MapView.swift
@@ -15,13 +15,89 @@ struct MapView: View {
     
     // 서울 좌표
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.5666791, longitude: 126.9782914), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    @State private var isLoggedIn: Bool = false
     
     var body: some View {
         NavigationView {
-            Map(coordinateRegion: $region,
-                showsUserLocation: false,
-                annotationItems: mockDatas){
-                data in MapMarker(coordinate: data.coordinate)
+            ZStack {
+                Map(coordinateRegion: $region,
+                    showsUserLocation: false,
+                    annotationItems: mockDatas){
+                    data in MapMarker(coordinate: data.coordinate)
+                }
+                
+                HStack {
+                    Spacer()
+                    
+                    VStack {
+                        Button(action: {}) {
+                            Image(systemName: "gearshape")
+                                .circleButton(
+                                    iconColor: .navWhite,
+                                    iconWidth: 20,
+                                    iconHeight: 20,
+                                    buttonColor: .primaryRed,
+                                    buttonSize: 50,
+                                    shadowRadius: 4,
+                                    shadowY: 4
+                                )
+                                .shadow(radius: 4, y: 4)
+                        }
+                        
+                        NavigationLink {
+                            ListView()
+                        } label: {
+                            Image(systemName: "list.bullet")
+                                .circleButton(
+                                    iconColor: .navWhite,
+                                    iconWidth: 20,
+                                    iconHeight: 14.5,
+                                    buttonColor: .primaryRed,
+                                    buttonSize: 50,
+                                    shadowRadius: 4,
+                                    shadowY: 4
+                                )
+                                .shadow(radius: 4, y: 4)
+                        }
+                        
+                        Spacer()
+                        
+                        if isLoggedIn {
+                            NavigationLink {
+                                PinCreationView()
+                            } label: {
+                                Image(systemName: "plus")
+                                    .circleButton(
+                                        iconColor: .navWhite,
+                                        iconWidth: 17,
+                                        iconHeight: 16,
+                                        buttonColor: .primaryRed,
+                                        buttonSize: 50,
+                                        shadowRadius: 4,
+                                        shadowY: 4
+                                    )
+                                    .shadow(radius: 4, y: 4)
+                            }
+                        } else {
+                            Button {
+                                isClickedYes = true
+                            } label: {
+                                Image(systemName: "plus")
+                                    .circleButton(
+                                        iconColor: .navWhite,
+                                        iconWidth: 17,
+                                        iconHeight: 16,
+                                        buttonColor: .primaryRed,
+                                        buttonSize: 50,
+                                        shadowRadius: 4,
+                                        shadowY: 4
+                                    )
+                                    .shadow(radius: 4, y: 4)
+                            }
+                        }
+                    }
+                    .padding(.trailing, 16)
+                }
             }
             .navigationTitle("NAV")
             .searchable(
