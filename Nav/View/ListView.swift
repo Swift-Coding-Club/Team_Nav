646ba70a48f4e5598f95886139d0b5d83e1025f2
taek0622 <min4644@gmail.com>
2023-05-19 19:59:20 +0900

    Design📱: #27 Editing ListView
    ListView Edit, SearchBar add, Utils Group add, Extensions add

--- Nav/View/ListView.swift
+++ Nav/View/ListView.swift
@@ -5,29 +5,68 @@
 //  Created by shingyuuser on 2022/11/06.
 //
 
+import MapKit
 import SwiftUI
 
 struct ListView: View {
     private var mockDatas: [MockDatum] = MockDatum.allData
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.5666791, longitude: 126.9782914), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    @State var searchText = ""
+    @State var inSearchMode = false
     
     var body: some View {
-        NavigationView {
+        
+        VStack(alignment: .leading){
+            
+            Text("Nav")
+                .font(.system(size: 40,weight: .semibold))
+                .padding(EdgeInsets(top: 0, leading: 30, bottom: -10, trailing: 0))
+            
+            SearchBar(text: $searchText, isEditing: $inSearchMode)
+                .padding()
+            
+            Map(coordinateRegion: $region,
+                showsUserLocation: false,
+                annotationItems: mockDatas){
+                data in MapMarker(coordinate: data.coordinate)
+            }
+            
+            Text("주소 리스트")
+                .font(.system(size: 20,weight: .semibold))
+                .padding(EdgeInsets(top: 0, leading: 150, bottom: 8, trailing: 0))
+            HStack {
+                Text("카페")
+                    .font(.system(size: 16,weight: .semibold))
+                    .foregroundColor(.indigo)
+                    .padding(EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 0))
+                Spacer()
+                Text("최신순")
+                    .font(.system(size: 16,weight: .semibold))
+                    .foregroundColor(.blue)
+                Image(systemName: "arrow.down.square")
+                    .foregroundColor(.blue)
+                    .padding(EdgeInsets(top: 0, leading: -7, bottom: 0, trailing: 20))
+            }
+            
             List{
                 ForEach(mockDatas, id: \.id) {
                     mockDatum in
-                    Text("\(mockDatum.title.rawValue)")
-                        .padding()
-                        .swipeActions {
-                            Button(role: .destructive) {
-                                // code
-                            } label: {
-                                Label("Delete", systemImage: "trash.fill")
+                    HStack{
+                        Image(systemName: "cup.and.saucer")
+                            .foregroundColor(.redBean)
+                        Text("\(mockDatum.title.rawValue)")
+                            .font(.system(size: 15))
+                            .swipeActions {
+                                Button(role: .destructive) {
+                                    // code
+                                } label: {
+                                    Label("Delete", systemImage: "trash.fill")
+                                }
                             }
-                        }
+                    }
                 }
             }
         }
-        .edgesIgnoringSafeArea(.all)
     }
 }
 
---
+++ Nav/View/SearchBar.swift
@@ -0,0 +1,53 @@
+//
+//  SearchBar.swift
+//  Nav
+//
+//  Created by Taewan Kim on 2023/05/19.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    @Binding var text: String
+    @Binding var isEditing: Bool
+    
+    var body: some View {
+        HStack {
+            TextField("Search...", text: $text)
+                .padding(8)
+                .padding(.horizontal,24)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 8)
+                    }
+                )
+                .onTapGesture {
+                    isEditing = true
+                }
+            
+            if isEditing {
+                Button {
+                    isEditing = false
+                    text = ""
+                    UIApplication.shared.endEditing()
+                } label: {
+                    Text("Cancel")
+                        .foregroundColor(.black)
+                }
+                .padding(.trailing, 8)
+                .transition(.move(edge: .trailing))
+            }
+        }
+    }
+}
+
+struct SearchBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBar(text: .constant("Search..."), isEditing: .constant(true))
+    }
+}
